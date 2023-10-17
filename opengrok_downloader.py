fcc452e3b2f07059ef32a1805c6f6b96b04713fe
AceSineX <75340781+AceSineX@users.noreply.github.com>
2023-10-17 07:05:02 +0300

    Account for subdirectories
    also added -r flag for recursive download

--- opengrok_downloader.py
+++ opengrok_downloader.py
@@ -1,11 +1,49 @@
 import requests
 import argparse
+import os
+
+def download_directory(url, path=""):
+    r = requests.get(url, allow_redirects=True)
+    wanted = r.text.split("tbody")[1].split("tbody")[0]
+    size = len(r.text.split("tbody")[1].split("tbody")[0].split("<tr><td><p class=\"")) - 1
+
+    for i in range(1, size):
+        filename = wanted.split("></td><td><a href=\"")[i].split("\"")[0]
+        link = url + filename
+        
+        # It is a directory
+        if link.endswith("/"):
+            if args.recursive:
+                if not args.quiet:
+                    print("Subdirectory : " + link)
+                if os.path.exists(filename):
+                    if not args.quiet:
+                        print(filename + " : EXISTS!")
+                else:
+                    os.makedirs(filename)
+                    if not args.quiet:
+                        print(filename + " : CREATED!")
+
+                download_directory(link, filename)
+
+        # It is a file
+        else:
+            final_dl_link = url.replace("xref", "raw") + filename
+            if not args.quiet:
+                print("Downloading : " + final_dl_link)
+
+            r1 = requests.get(final_dl_link, allow_redirects=True)
+            with open(path + filename, 'wb') as f:
+                f.write(r1.content)
+
 
 parser = argparse.ArgumentParser(description='Simple OpenGrok directory downloader')
 parser.add_argument("opengrok_url", help="The URL of the OpenGrok directory to download from")
 
 # Define a "quiet" flag
 parser.add_argument("-q", "--quiet", action="store_true", help="Suppress output (quiet mode)")
+# Define recursive flag
+parser.add_argument("-r", "--recursive", action="store_true", help="Recursively download all subdirectories")
 
 args = parser.parse_args()
 
@@ -14,22 +52,9 @@ if not args.opengrok_url:
     url = args.opengrok_urlddq
     sys.exit(1)
 
-url = args.opengrok_url + "/"
+url = args.opengrok_url
 
-r = requests.get(url, allow_redirects=True)
+if not url.endswith("/"):
+    url = url + "/"
 
-wanted = r.text.split("tbody")[1].split("tbody")[0]
-a = wanted.split("<tr><td><p class=\"p\"/></td><td><a href=")[1].split("\"")[1]
-
-size = len(r.text.split("tbody")[1].split("tbody")[0].split("<tr><td><p class=\"p\"/></td><td><a href="))
-
-for i in range(1, size):
-    filename = wanted.split("<tr><td><p class=\"p\"/></td><td><a href=")[i].split("\"")[1]
-    final_dl_link = url.replace("xref", "raw") + filename
-
-    if not args.quiet:
-        print("Downloading : " + final_dl_link)
-
-    r1 = requests.get(final_dl_link, allow_redirects=True)
-    with open(filename, 'wb') as f:
-            f.write(r1.content)
+download_directory(url)
